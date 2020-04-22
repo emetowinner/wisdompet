d984dd5355a5cac084e9dfad1e1a57da00edf485
emetowinner <emetowinner@gmail.com>
2020-04-22 09:22:10 +0100

    Add templates and static files tothe project and modify the Html documents

--- wisdompet/adoptions/views.py
+++ wisdompet/adoptions/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+from .models import Pet
+
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request,'adoptions/home.html',{'pets':pets})
+
+def pet_details(request,id):
+    try:
+        pet = Pet.objects.get(id=id)
+    except:
+        raise Http404('Pet not found')
+        
+    return render(request,'adoptions/pet_details.html',{'pet':pet})
+
--- wisdompet/wisdompet/urls.py
+++ wisdompet/wisdompet/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from adoptions import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^adoptions/(\d+)/$', views.pet_details, name='pet_details'),
 ]
